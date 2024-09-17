ccba4c151fead992b8ee8b310ce13db9830b575a
Sivagajan.Karunanathan <sivagajan@gmail.com>
2024-09-17 08:57:40 +0200

    ext_emconf and composer.json added

---
+++ ext_emconf.php
@@ -0,0 +1,20 @@
+<?php
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Siva Sitepackage',
+    'description' => 'Kickstarter Sitepackage to Create Websites based on Bootstrap',
+    'category' => 'plugin',
+    'author' => 'Sivagajan Karunanathan',
+    'author_email' => 'sivagajan@gmail.com',
+    'author_company' => 'sivtech',
+    'state' => 'alpha',
+    'version' => '0.0.1',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '12.4-13.0',
+        ],
+        'conflicts' => [
+        ],
+        'suggests' => [
+        ],
+    ],
+];
