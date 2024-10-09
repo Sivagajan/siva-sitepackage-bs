e1bbec62c94373e3c7234e624b86e1341ec88f52
Sivagajan.Karunanathan <sivagajan@gmail.com>
2024-10-09 10:40:21 +0200

    webpack configurations added

--- Build/webpack.config.js
+++ Build/webpack.config.js
@@ -1,10 +1,46 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development', // use 'production' for production mode
+  entry: {
+    bootstrapMain: './JS/Bootstrap/main.js', // bundle 1
+    test: './JS/Custom/test.js',             // bundle 2
+  },
   output: {
-    filename: 'my-first-webpack.bundle.js',
+    filename: '[name].js',
+    path: path.resolve(__dirname, '../Resources/Public/JS'), 
+  },
+  resolve:{
+    alias:{
+      // This allows you to use import bootstrap from ‘bootstrap’
+      // in your JavaScript files without having to specify the full path.
+      bootstrap: path.resolve(__dirname, 'node_modules/bootstrap')      
+    }
   },
   module: {
-    rules: [{ test: /\.txt$/, use: 'raw-loader' }],
-  },
-};
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              sassOptions: {
+                quietDeps: true,
+              },
+            },
+          },
+        ],
+      },
+    ]
+  }
+}
