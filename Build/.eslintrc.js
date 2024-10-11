5d040a4cbe424e32303a26da858c96729e93a0cb
Sivagajan.Karunanathan <sivagajan@gmail.com>
2024-10-11 07:59:36 +0200

    feat: eslint configuration added

---
+++ Build/.eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true,
+    'node': true
+  },
+  'extends': 'eslint:recommended',
+  'overrides': [
+    {
+      'env': {
+        'node': true
+      },
+      'files': [
+        '.eslintrc.{js,cjs}'
+      ],
+      'parserOptions': {
+        'sourceType': 'script'
+      }
+    }
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module'
+  },
+  'rules': {
+     // Individual rules can be added or customised here
+    'indent': ['error', 2], // Example: Use 2 space indentation
+    'quotes': ['error', 'single'], // Example: Use single inverted commas
+    'semi': ['error', 'always'], // Example: Always use a semicolon at the end
+    'no-unused-vars': 'off' // Deactivate no-unused-vars rule for the entire project
+  }
+};
